e325598831d4fefcf60776596841c7b1ea5aa791
Nuruzzamanshah <mnueuzzaman@gmail.com>
2022-05-27 19:47:42 +0600

    user added

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function run(){
             res.send(purchases);
         });
 
-        app.get('/user',async(req, res) =>{
+        app.get('/user', async(req, res) =>{
           const users = await usersCollection.find().toArray();
           res.send(users);
         });
@@ -45,7 +45,19 @@ async function run(){
           const result = await usersCollection.updateOne(filter, updateDoc, options);
           const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
           res.send({result, token});
+        });
+
+
+        app.put('/user/:admin/:email', async(req, res)=>{
+          const email = req.params.email;
+          const filter = {email: email}
+          const updateDoc = {
+            $set: {role:'admin'},
+          };
+          const result = await usersCollection.updateOne(filter, updateDoc);
+          res.send(result);
         })
+
 
         app.get('/booking', async(req, res) =>{
           const userEmail = req.query.userEmail;
